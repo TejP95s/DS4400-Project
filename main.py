687278729942f4d6da2e4447a72d2ff6944b23d9
TejP95s <patel.tej5@northeastern.edu>
2026-03-05 15:29:35 -0500

    added decision tree model and k-fold CV

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from sklearn.model_selection import train_test_split
-import numpy as np
 
 from src.data import load_data, basic_cleaning
 from src.features import feature_engineering, preprocess
 from src.models import get_models
 from src.evaluate import evaluate_model
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold, cross_val_score
+import numpy as np
 
+
+def cross_validate_model(model, X, y, k=5):
+    kf = KFold(n_splits=k, shuffle=True, random_state=42)
+
+    scores = cross_val_score(
+        model,
+        X,
+        y,
+        scoring="neg_root_mean_squared_error",
+        cv=kf
+    )
+
+    rmse_scores = -scores
+    return {
+        "CV_RMSE_mean": np.mean(rmse_scores),
+        "CV_RMSE_std": np.std(rmse_scores)
+    }
 
 def main():
     df = load_data("data/vehicles.csv")
@@ -15,7 +33,7 @@ def main():
 
     df = feature_engineering(df)
 
-    y = np.log1p(df["price"])
+    y = df["price"]
     X = df.drop(columns=["price"])
     
 
@@ -36,15 +54,21 @@ def main():
     all_results = {}
 
     for name, model in models.items():
+        cv_results = cross_validate_model(model, X_train, y_train, k=5)
         results = evaluate_model(model, X_train, X_test, y_train, y_test)
-        all_results[name] = results
+
+        all_results[name] = {**results, **cv_results}
 
         print(f"\n{name}")
+
+        print(f"CV RMSE Mean: {cv_results['CV_RMSE_mean']:.4f}")
+        print(f"CV RMSE Std: {cv_results['CV_RMSE_std']:.4f}")
+
         for metric, value in results.items():
             print(f"{metric}: {value:.4f}")
 
     model_names = list(all_results.keys())
-    rmse_scores = [results["RMSE"] for results in all_results.values()]
+    rmse_scores = [results["CV_RMSE_mean"] for results in all_results.values()]
     
     plt.bar(model_names, rmse_scores)
     plt.xticks(rotation=45)
@@ -57,21 +81,36 @@ if __name__ == "__main__":
     main()
 
 '''Initial results
+
 Ridge
-MSE: 0.6793
-RMSE: 0.8242
-MAE: 0.5356
-R2: 0.3590
+CV RMSE Mean: 10125.2034
+CV RMSE Std: 128.7503
+MSE: 100705300.2353
+RMSE: 10035.2030
+MAE: 6959.1805
+R2: 0.4814
+
+Decision Tree
+CV RMSE Mean: 6519.8939
+CV RMSE Std: 81.0203
+MSE: 39592290.8362
+RMSE: 6292.2405
+MAE: 3108.9266
+R2: 0.7961
 
 Random Forest
-MSE: 0.2987
-RMSE: 0.5466
-MAE: 0.2422
-R2: 0.7181
+CV RMSE Mean: 5056.4718
+CV RMSE Std: 81.2347
+MSE: 24183928.5569
+RMSE: 4917.7158
+MAE: 2249.3165
+R2: 0.8755
 
 Neural Network
-MSE: 0.7813
-RMSE: 0.8839
-MAE: 0.5657
-R2: 0.2627
+CV RMSE Mean: 7563.7183
+CV RMSE Std: 298.1698
+MSE: 54615751.7988
+RMSE: 7390.2471
+MAE: 4585.3100
+R2: 0.7187
 '''
--- src/models.py
+++ src/models.py
@@ -1,11 +1,13 @@
 from sklearn.linear_model import Ridge
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 
 def get_models():
-    models = { #will add 4th model here soon.
+    models = {
         "Ridge": Ridge(alpha=1.0),
+        "Decision Tree": DecisionTreeRegressor(max_depth=20, random_state=42),
         "Random Forest": RandomForestRegressor(n_estimators=100, random_state=42),
         "Neural Network": MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=500)
     }
